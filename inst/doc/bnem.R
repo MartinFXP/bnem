8d9ff5bab0ea7b9dafa3379701b5e193b8b6ced9
MartinFXP <martin.pirkl@bsse.ethz.ch>
2016-11-09 15:26:19 +0100

    updated vignette

--- inst/doc/bnem.R
+++ inst/doc/bnem.R
@@ -367,7 +367,7 @@ ERS.res <- computeFc(CNOlist, t(simulateStatesRecursive(CNOlist, model, resStrin
 ERS.res <- ERS.res[, which(colnames(ERS.res) %in% colnames(ERS))]
 print(sum(ERS.res == ERS)/length(ERS))
 
-## ----residuals, fig.width=7, fig.height=5, out.width='0.9\\linewidth'-------------------
+## ----residuals--------------------------------------------------------------------------
 residuals <- findResiduals(resString3, CNOlist, model, fc, verbose = F) # verbose = TRUE plots the residuals matrices
 
 ## ----loadbcrdata------------------------------------------------------------------------
